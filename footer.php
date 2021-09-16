57300ce729b184cf82475243c4d22e763ab6fe5b
unknown <amsalutilaye2019@gmail.com>
2021-09-16 14:40:15 +0300

    the start of the project on 16/9/2021 - header, footer and index created.

---
+++ footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+    <style>
+       #textbox{
+            height:auto;
+            width:auto;
+            background:#00b300;
+            margin: auto;
+            padding: auto;
+            vertical-align:center;
+            color:black;
+            font-family:Serif;
+            font-weight:bolder;
+            opacity:0.7;
+            border-radius:5px;
+            text-align:center
+        }
+    </style>
+    <script src="jquery/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div id="textbox">
+            <p><span>&#169;</span>2021 All Rights Reserved</p>
+    </div>
+    <div style="clear: both;"></div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-grid.min.css">
+    <style>
+        .container-fluid{
+            height:auto;
+            width:auto;
+            background:#00b300;
+            margin: 10px 10px 10px 5px;
+            padding: auto;
+            vertical-align:center;
+            color:black;
+            font-family:Serif;
+        }
+        .container-fluid a{
+            color:brown;
+            font-weight:bold;
+            border-radius:5px;
+        }
+        .container-fluid h4{
+            color:black;
+            text-align:center;
+            font-weight:bolder;
+        }
+    </style>
+    <script src="jquery/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10">
+                <h4>NDMC Geospatial Vizualization</h4>
+            </div>
+            <div class="col-md-1">
+                <a href="#">NDMC</a>
+            </div>
+            <div class="col-md-1">
+                <a href="#">RTDS</a>
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
